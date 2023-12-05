c4dba617153ed1cf0b58c1e22094857cf3e89524
Mario <maalnu93@hotmail.com>
2023-12-05 10:51:38 +0100

    Clase productManager

---
+++ scripts/productManager.js
@@ -0,0 +1,56 @@
+
+
+export class ProductManager{
+
+    #products;
+
+    constructor(){
+        this.#products = [];
+    }
+
+    //Metodo para obtener la lista de productos
+    listProducts(){
+        return this.#products;
+    }
+
+    //Metodo para agregar un producto a la lista
+    addProduct(product){
+        this.#products.push(product);
+    }
+
+    //Metodo para actualizar un producto de la lista
+    updateProductById(id, updateProduct){
+        const index = this.#products.findIndex(product => product.id === id);
+        //Si no existe error (es coincidente el index)
+        if(index !== -1){
+            this.#products[index] = updateProduct;
+        }
+        
+    }
+
+    //Metodo para eliminar un producto de la lista
+    deleteProductById(id){
+        const index = this.#products.findIndex(product => product.id === id);
+        //Si no existe error (es coincidente el index)
+        if(index !== -1){
+            this.#products.splice(index, 1);
+        }
+    }
+
+    //Metodo para mostrar todos los productos dentro del array
+    showProducts(){
+        for(const product of this.#products){
+            console.log(`ID: ${product.id} - Nombre: ${product.nombre} - Precio: ${product.precio} - Cantidad: ${product.cantidad}`);
+        };
+    }
+
+    //Getters y Setters
+
+    get products(){
+        return this.#products;
+    }
+
+    set products(products){
+        this.#products = products;
+    }
+}
