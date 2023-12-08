d0881316c52681d530e386926a015ae841509b67
mario <maalnu93@hotmail.com>
2023-12-08 12:12:29 +0100

    Boton atras funcion editar

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ document.getElementById('product-form-events').addEventListener('submit', functi
     const productName = document.getElementById('product-name').value;
     const productPrice = document.getElementById('product-price').value;
     const productQuantity = parseInt(document.getElementById('product-quantity').value);
-    
+
 
     //Crea un nuevo producto
 
@@ -33,7 +33,7 @@ document.getElementById('product-form-events').addEventListener('submit', functi
 
     //Actualiza la tabla del inventario
 
-    updateInventoryTable();    
+    updateInventoryTable();
 
 });
 
@@ -42,7 +42,7 @@ document.getElementById('product-form-events').addEventListener('submit', functi
 function updateInventoryTable() {
     const inventoryTable = document.getElementById('body-table');
     inventoryTable.innerHTML = "";
-    
+
 
     productManager.products.forEach(product => {
         inventoryTable.innerHTML += `
@@ -80,15 +80,15 @@ function updateInventoryTable() {
             event.preventDefault();
             const id = parseInt(this.dataset.id);
 
-            const newProduct = new Product(id, prompt("Introduzca nuevo nombre"), prompt("Introduzca nueva cantidad"),prompt("Introduzca nuevo precio"));
+            const newProduct = new Product(id, prompt("Introduzca nuevo nombre"), prompt("Introduzca nueva cantidad"), prompt("Introduzca nuevo precio"));
 
-            
+
             productManager.updateProductById(id, newProduct);
             inventoryTable.innerHTML = "";
             updateInventoryTable();
         });
     });
-    
+
 
     //Evento para buscar un producto
 
@@ -98,9 +98,67 @@ function updateInventoryTable() {
         const product = productManager.searchProductByName(productName);
         if (product) {
             console.log(product);
-            alert(`ID: ${product.id} - Nombre: ${product.nombre} - Precio: ${product.precio} - Cantidad: ${product.cantidad}`);
+            inventoryTable.innerHTML = `
+                <tr>
+                    <td>${product.nombre}</td>
+                    <td>${product.cantidad}</td>
+                    <td>${product.precio}</td>
+                    
+                    <td>
+                        <a href="#" class="btn-delete" name="delete-product" data-id="${product.id}">Delete</a>
+                        <a href="#" class="btn-edit" name="edit-product" data-id="${product.id}">Editar</a>
+                    </td>
+                </tr>
+            `;
+
+            //Evento para eliminar un producto
+
+            document.getElementsByName('delete-product').forEach(element => {
+                element.addEventListener('click', function (event) {
+                    console.log('Eliminando producto');
+                    event.preventDefault();
+                    const id = parseInt(this.dataset.id);
+                    productManager.deleteProductById(id);
+                    inventoryTable.innerHTML = "";
+                    updateInventoryTable();
+                });
+            });
+
+            //Evento para editar un producto
+
+            document.getElementsByName('edit-product').forEach(element => {
+                element.addEventListener('click', function (event) {
+                    console.log('Editando producto');
+                    event.preventDefault();
+                    const id = parseInt(this.dataset.id);
+
+                    const newProduct = new Product(id, prompt("Introduzca nuevo nombre"), prompt("Introduzca nueva cantidad"), prompt("Introduzca nuevo precio"));
+
+
+                    productManager.updateProductById(id, newProduct);
+                    inventoryTable.innerHTML = "";
+                    updateInventoryTable();
+                });
+            });
+
+            // Crear el botón "Atrás" si no existe
+            if (!document.getElementById('back-button')) {
+                const backButton = document.createElement('button');
+                backButton.textContent = 'Atrás';
+                backButton.id = 'back-button';
+                backButton.className = 'back-button';
+
+                // Agregar el botón al DOM
+                document.body.appendChild(backButton);
+
+                // Agregar el evento de clic
+                backButton.addEventListener('click', function(event){
+                    event.preventDefault();
+                    updateInventoryTable();
+                });
+            }
+
         } else {
             alert('Producto no encontrado');
         }
-    });
-}
+    })};
